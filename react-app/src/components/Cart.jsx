15e89204ab248c35cb06b099c61a0042aed98b4b
eris <erisismajli7@gmail.com>
2026-01-12 20:19:01 +0100

    total price, add new product

--- react-app/src/components/Cart.jsx
+++ react-app/src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./simpleStore.css";
 import { useNavigate } from "react-router-dom";
 
@@ -6,28 +6,42 @@ const Cart = ({ cartProducts, setCartProducts }) => {
   const navigate = useNavigate();
 
   const deleteCartProduct = (index) => {
-    const updatedCartProducts = cartProducts.filter((product, i) => i !== index)
-    setCartProducts(updatedCartProducts)
-  }
+    const updatedCartProducts = cartProducts.filter(
+      (product, i) => i !== index
+    );
+    setCartProducts(updatedCartProducts);
+  };
+
+  const totalPrice = cartProducts.reduce(
+    (total, product) => total + product.price,
+    0
+  );
 
   return (
     <div>
       <header>
         <h1>Your Cart</h1>
+        <h3>Total price: ${totalPrice}</h3>
         <button onClick={() => navigate("/")}>Continue Shopping</button>
       </header>
       <section className="products-list">
-        {cartProducts.map((product, index) => {
-          return (
-            <div key={index} className="product-card">
-              <img src={product.image} />
-              <p>{product.category}</p>
-              <h3>{product.title}</h3>
-              <h3>${product.price}</h3>
-              <button onClick={() => deleteCartProduct(index)}>Delete from cart</button>
-            </div>
-          );
-        })}
+        {cartProducts.length > 0 ? (
+          cartProducts.map((product, index) => {
+            return (
+              <div key={index} className="product-card">
+                <img src={product.image} />
+                <p>{product.category}</p>
+                <h3>{product.title}</h3>
+                <h3>${product.price}</h3>
+                <button onClick={() => deleteCartProduct(index)}>
+                  Delete from cart
+                </button>
+              </div>
+            );
+          })
+        ) : (
+          <p>No items in your cart.</p>
+        )}
       </section>
     </div>
   );
--- react-app/src/components/SimpleStore.jsx
+++ react-app/src/components/SimpleStore.jsx
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from "react";
 import "./simpleStore.css";
 import { useNavigate } from "react-router-dom";
 
-const SimpleStore = ({cartProducts, setCartProducts}) => {
-  const navigate = useNavigate()
+const SimpleStore = ({ cartProducts, setCartProducts }) => {
+  const navigate = useNavigate();
 
   const [products, setProducts] = useState([]);
+  const [newProductData, setNewProductData] = useState({
+    title: "",
+    category: "",
+    price: null,
+    image: "",
+  });
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -15,16 +21,41 @@ const SimpleStore = ({cartProducts, setCartProducts}) => {
   }, []);
 
   const addToCart = (product) => {
-    setCartProducts(prev => ([
-        ...prev,
-        product
-    ]))
-  }
+    setCartProducts((prev) => [...prev, product]);
+  };
 
   const deleteProduct = (index) => {
-    const updatedProducts = products.filter((product, i) => i !== index)
-    setProducts(updatedProducts)
-  }
+    const updatedProducts = products.filter((product, i) => i !== index);
+    setProducts(updatedProducts);
+  };
+
+  const handleChange = (e) => {
+    const { name, value, files, type } = e.target;
+
+    setNewProductData((prev) => ({
+      ...prev,
+      [name]: type === "file" ? files[0] : value,
+    }));
+  };
+
+  const addProduct = () => {
+    const { title, category, price, image } = newProductData;
+
+    if (title.trim() === "" || category.trim() === "" || price === null) {
+      alert("Please provide all the data");
+      return;
+    }
+
+    const productToAdd = {
+      id: Date.now(),
+      title,
+      category,
+      price,
+      image,
+    };
+
+    setProducts((prev) => [...prev, productToAdd]);
+  };
 
   return (
     <main>
@@ -32,16 +63,45 @@ const SimpleStore = ({cartProducts, setCartProducts}) => {
         <h1>Simple store</h1>
 
         <div className="cart">
-            <button onClick={() => navigate("/cart")}>Cart</button>
-            <p>{cartProducts.length}</p>
+          <button onClick={() => navigate("/cart")}>Cart</button>
+          <p>{cartProducts.length}</p>
         </div>
       </header>
+
+      <input
+        onChange={handleChange}
+        type="text"
+        name="title"
+        placeholder="new product title"
+      />
+      <input
+        onChange={handleChange}
+        type="text"
+        name="category"
+        placeholder="new product category"
+      />
+      <input
+        onChange={handleChange}
+        type="number"
+        name="price"
+        placeholder="new product price"
+      />
+      <input onChange={handleChange} type="file" name="image" accept="/image" />
+      <button onClick={addProduct}>Add product</button>
 
       <section className="products-list">
         {products.map((product, index) => {
           return (
             <div key={index} className="product-card">
-              <img src={product.image} />
+              <img
+                src={
+                  product.image instanceof File
+                    ? URL.createObjectURL(product.image)
+                    : product.image
+                }
+                alt={product.title}
+              />
+
               <p>{product.category}</p>
               <h3>{product.title}</h3>
               <h3>${product.price}</h3>
